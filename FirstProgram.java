4f7df0457ccee07ae919fcc0fe65ae1d4c217378
MohdIrfan03 <irfan.miu222@gmail.com>
2025-11-20 21:09:49 +0530

    20-11-2025

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+        System.out.println("my name is "+args[0]);
+        System.out.println("helloworld");
+    }
+}
